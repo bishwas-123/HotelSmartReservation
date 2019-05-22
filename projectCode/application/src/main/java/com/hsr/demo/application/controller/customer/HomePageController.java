50f7686e3e71005163e782e3de61be41a93af274
Bishwas Niraula <bishwas.niraula@gmail.com>
2019-05-21 23:01:02 -0500

    changes on master layout

--- projectCode/application/src/main/java/com/hsr/demo/application/controller/customer/HomePageController.java
+++ projectCode/application/src/main/java/com/hsr/demo/application/controller/customer/HomePageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomePageController {
-    @RequestMapping(value={"/","/user","user","customer"},
+    @RequestMapping(value={"/enduser","/user","user","customer"},
             method= RequestMethod.GET)
     public String homepage() {
         return "customer/index";
