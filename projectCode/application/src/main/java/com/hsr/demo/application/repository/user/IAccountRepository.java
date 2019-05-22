f9caf8aeed9aca8afb05c45eedf07088fbce38cb
Bishwas Niraula <bishwas.niraula@gmail.com>
2019-05-21 23:04:36 -0500

    test

--- projectCode/application/src/main/java/com/hsr/demo/application/repository/user/IAccountRepository.java
+++ projectCode/application/src/main/java/com/hsr/demo/application/repository/user/IAccountRepository.java
@@ -3,5 +3,5 @@ package com.hsr.demo.application.repository.user;
 import com.hsr.demo.application.model.user.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IAccountRepository extends JpaRepository<Account, Integer> {
+public interface IAccountRepository extends JpaRepository<Account,Integer> {
 }
